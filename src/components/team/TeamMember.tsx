15bab849d15095680143bf96e86efbaffb0432ed
Nick Nijenhuis <nick@novana.nl>
2023-05-15 15:24:32 +0200

    Add team page as a seperate page

---
+++ src/components/team/TeamMember.tsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import { Icon } from "@iconify/react";
+
+interface Props {
+    data: any
+}
+
+const TeamMember = ({ data }: Props) => {
+    return (
+      <div className="lg:pb-[60px] pb-[45px] team-member">
+        <div className="member-wrapper aspect-square flex items-center justify-center p-2 w-full max-h-[405px] h-full relative max-w-[405px] overflow-hidden mx-auto">
+          <Image
+            width="0"
+            height="0"
+            src={data.img.data.attributes.url}
+            alt="test"
+            sizes="404px"
+            priority
+            className="w-full rounded-full"
+            blurDataURL={data.img.data.attributes.url} />
+        </div>
+        <a
+          href={`mail:${data.email}`}
+          className="w-full bg-dark-purple font-semibold ease-linear duration-300 items-center justify-between p-3 mt-6 rounded-full pr-2 pl-8 normal-case hover:bg-light-purple focus:bg-light-purple h-auto inline-flex gap-8 2xl:text-[30px] text-lg text-white"
+        >
+          {data.name}
+          <span className="block p-3 bg-white rounded-full">
+            <Icon
+              icon="material-symbols:mail-rounded"
+              color="#2C2E80"
+              width="20"
+              height="20" />
+          </span>
+        </a>
+      </div>
+    );
+  }
+
+  export default TeamMember;
---
+++ src/pages/team.tsx
@@ -0,0 +1,45 @@
+import Footer from "@/components/Footer";
+import NavSection from "@/components/Header";
+import TeamsBlock from "@/components/main-page/TeamsBlock";
+import TeamsGrid from "@/components/team/TeamsGrid";
+import { GET_TEAM_DATA } from "@/graphql/queries";
+import { HomePage } from "@/types/types";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { GetServerSideProps } from "next";
+import Head from 'next/head'
+
+export default function Home(props: HomePage) {
+  const { header, footer } = props
+  const {
+    teams,
+    contactsInfo,
+    seo  } = props.home.data.attributes;
+
+  return (
+    <>
+      <Head>
+        <title>{seo.metaTitle}</title>
+        <meta name='description' content={seo.metaDescription} />
+        <link rel="canonical" href={seo.canonicalURL} />
+      </Head>
+      <NavSection data={header} info={contactsInfo} />
+      <TeamsGrid data={teams.data}/>
+      <Footer data={footer} info={contactsInfo} />
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const client = new ApolloClient({
+    uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`,
+		cache: new InMemoryCache(),
+	})
+
+	const { data } = await client.query({
+		query: GET_TEAM_DATA
+	})
+
+	return {
+		props: data
+  }
+}
