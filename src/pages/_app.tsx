7625cde35bc6f7bac62ea2cf3c98b14c0b763995
Astapov, Pavel <p.astapov@itransition.com>
2023-07-22 10:08:56 +0300

    added GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,10 @@ import { useEffect } from "react";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log(process.env.NEXT_PUBLIC_GTM_ID);
   useEffect(() => {
     TagManager.initialize({ gtmId: `${process.env.NEXT_PUBLIC_GTM_ID}` });
   }, []);
+
   return (
     <>
       <div className={montserrat.className}>
