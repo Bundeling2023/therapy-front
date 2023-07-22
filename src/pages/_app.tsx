85e7816d9443d5fc88fb69a87cfbd751f178e0b3
Astapov, Pavel <p.astapov@itransition.com>
2023-07-22 10:06:43 +0300

    added GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import NextNProgress from "nextjs-progressbar";
 import { Montserrat } from "next/font/google";
+import TagManager from 'react-gtm-module';
+import { useEffect } from "react";
 
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log(process.env.NEXT_PUBLIC_GTM_ID);
+  useEffect(() => {
+    TagManager.initialize({ gtmId: `${process.env.NEXT_PUBLIC_GTM_ID}` });
+  }, []);
   return (
     <>
       <div className={montserrat.className}>
