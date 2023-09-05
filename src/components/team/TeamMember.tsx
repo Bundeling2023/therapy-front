5d4006ce9998245c4c4d01645042dbf8d767fd75
Nick Nijenhuis <nick@novana.nl>
2023-09-05 23:21:40 +0200

    Improve team member linking

--- src/components/team/TeamMember.tsx
+++ src/components/team/TeamMember.tsx
@@ -6,30 +6,29 @@ import HTMLReactParser from "html-react-parser";
 import { useState } from "react";
 
 interface Props {
-  data: Team,
-  isMain?: boolean,
+  data: Team;
+  isMain?: boolean;
 }
 
 function getInitials(fullName: string) {
-  const [firstName, ...restNames] = fullName.toUpperCase().trim().split(' ')
+  const [firstName, ...restNames] = fullName.toUpperCase().trim().split(" ");
 
   if (!restNames.length) {
-    return firstName.substring(0,2)
+    return firstName.substring(0, 2);
   }
 
-  const firstNameInitial = firstName[0]
-  const lastNameInitial = restNames.pop()?.[0]
+  const firstNameInitial = firstName[0];
+  const lastNameInitial = restNames.pop()?.[0];
 
-  return `${firstNameInitial}${lastNameInitial}`
+  return `${firstNameInitial}${lastNameInitial}`;
 }
 
 const TeamMember = ({ data, isMain = false }: Props) => {
-
-  return (
-    <div className="lg:pb-[60px] pb-[45px] team-member">
-      <div className="member-wrapper h-auto aspect-square flex items-center justify-center p-2 w-full max-h-[405px] relative max-w-[405px] overflow-hidden mx-auto">
-        {data.attributes.img.data?.attributes ? (<Image
-          id={data.attributes.url.slice(data.attributes.url.indexOf('#') + 1)}
+  const teamMemberImage = (
+    <div className="member-wrapper h-auto aspect-square flex items-center justify-center p-2 w-full max-h-[405px] relative max-w-[405px] overflow-hidden mx-auto">
+      {data.attributes.img.data?.attributes ? (
+        <Image
+          id={generateTeamId(data.attributes.url)}
           width="0"
           height="0"
           src={data.attributes.img.data?.attributes.url}
@@ -37,43 +36,85 @@ const TeamMember = ({ data, isMain = false }: Props) => {
           sizes="404px"
           priority
           className="object-cover w-full h-full rounded-full"
-          blurDataURL={data.attributes.img.data?.attributes.url} />) : 
-<div className="relative inline-flex items-center justify-center w-4/6 h-4/6 overflow-hidden bg-[#827CB1] rounded-full">
-    <span className="text-gray-900 text-7xl font-semibold ease-linear tracking-widest	">{getInitials(data.attributes.name)}</span>
-</div>          } 
-      </div>
-      <div
-        className="w-full bg-dark-purple font-semibold ease-linear duration-300 items-center justify-between p-3 mt-6 rounded-full pr-2 pl-8 normal-case h-auto inline-flex gap-5 2xl:text-[30px] text-lg text-white"
-      >
-        {isMain ? <Link href={`${data.attributes.url}`}>{data.attributes.name}</Link> : data.attributes.name}
+          blurDataURL={data.attributes.img.data?.attributes.url}
+        />
+      ) : (
+        <div
+          id={generateTeamId(data.attributes.url)}
+          className="relative inline-flex items-center justify-center w-4/6 h-4/6 overflow-hidden bg-[#827CB1] rounded-full"
+        >
+          <span className="text-gray-900 text-7xl font-semibold ease-linear tracking-widest	">
+            {getInitials(data.attributes.name)}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+
+  return (
+    <div className="lg:pb-[60px] pb-[45px] team-member">
+      {isMain ? (
+        <Link href={`team#${data.attributes.url}`}>{teamMemberImage}</Link>
+      ) : (
+        <>{teamMemberImage}</>
+      )}
+      <div className="w-full bg-dark-purple font-semibold ease-linear duration-300 items-center justify-between p-3 mt-6 rounded-full pr-2 pl-8 normal-case h-auto inline-flex gap-5 2xl:text-[30px] text-lg text-white">
+        {isMain ? (
+          <Link href={`team#${data.attributes.url}`}>
+            {data.attributes.name}
+          </Link>
+        ) : (
+          data.attributes.name
+        )}
         <div className="flex gap-x-3">
-          <a href={`tel:${data.attributes.phone}`} className="block p-3 bg-white rounded-full" >
+          <a
+            href={`tel:${data.attributes.phone}`}
+            className="block p-3 bg-white rounded-full"
+          >
             <Icon
               icon="solar:phone-bold"
               color="#2C2E80"
               width="20"
-              height="20" />
+              height="20"
+            />
           </a>
-          <a href={`mailto:${data.attributes.email}`} className="block p-3 bg-white rounded-full" >
+          <a
+            href={`mailto:${data.attributes.email}`}
+            className="block p-3 bg-white rounded-full"
+          >
             <Icon
               icon="material-symbols:mail-rounded"
               color="#2C2E80"
               width="20"
-              height="20" />
+              height="20"
+            />
           </a>
         </div>
       </div>
       {!isMain && (
         <>
-          <ContactTable email={data.attributes.email} phone={data.attributes.phone} bigRegistrationNumber={data.attributes.bigRegistrationNumber} />
-          <div className="mt-5 text-lg team-desc" data-id={data.attributes.url.slice(data.attributes.url.indexOf('#') + 1)}>
+          <ContactTable
+            email={data.attributes.email}
+            phone={data.attributes.phone}
+            bigRegistrationNumber={data.attributes.bigRegistrationNumber}
+          />
+          <div
+            className="mt-5 text-lg team-desc"
+            data-id={data.attributes.url.slice(
+              data.attributes.url.indexOf("#") + 1
+            )}
+          >
             {HTMLReactParser(data.attributes.desc)}
           </div>
         </>
       )}
     </div>
   );
-}
+
+  function generateTeamId(url: string): string | undefined {
+    return url.slice(url.indexOf("#") + 1);
+  }
+};
 
 export default TeamMember;
 
@@ -83,23 +124,42 @@ interface ContactTableProps {
   bigRegistrationNumber?: string;
 }
 
-const ContactTable = ({ email, phone, bigRegistrationNumber }: ContactTableProps) => {
+const ContactTable = ({
+  email,
+  phone,
+  bigRegistrationNumber,
+}: ContactTableProps) => {
   const [showTable, setShowTable] = useState(false);
 
   if (!showTable) {
-    return <button className="mt-2 text-blue-500 hover:underline cursor-pointer text-sm text-center w-full" onClick={() => setShowTable(true)}>Contact info weergeven</button>
+    return (
+      <button
+        className="mt-2 text-blue-500 hover:underline cursor-pointer text-sm text-center w-full"
+        onClick={() => setShowTable(true)}
+      >
+        Contact info weergeven
+      </button>
+    );
   }
 
   return (
     <>
-      <button className="mt-2 text-blue-500 hover:underline cursor-pointer text-sm text-center w-full" onClick={() => setShowTable(false)}>Contact info verbergen</button>
+      <button
+        className="mt-2 text-blue-500 hover:underline cursor-pointer text-sm text-center w-full"
+        onClick={() => setShowTable(false)}
+      >
+        Contact info verbergen
+      </button>
       <div className="mt-5 p-2 break-words rounded-lg bg-zinc-100">
         <div className="grid grid-cols-3 lg:grid-cols-4 gap-y-2 text-lg">
           {email && (
             <>
               <div>E-mail:</div>
               <div className="col-span-2 lg:col-span-3">
-                <a href={`mailto:${email}`} className="text-blue-500 hover:underline">
+                <a
+                  href={`mailto:${email}`}
+                  className="text-blue-500 hover:underline"
+                >
                   {email}
                 </a>
               </div>
@@ -109,7 +169,10 @@ const ContactTable = ({ email, phone, bigRegistrationNumber }: ContactTableProps
             <>
               <div>Tel:</div>
               <div className="col-span-2 lg:col-span-3">
-                <a href={`tel:${phone}`} className="text-blue-500 hover:underline">
+                <a
+                  href={`tel:${phone}`}
+                  className="text-blue-500 hover:underline"
+                >
                   {phone}
                 </a>
               </div>
