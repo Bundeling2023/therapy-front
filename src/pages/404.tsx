85c974f4456200bc47847adba13533ee5a221001
nicknijenhuis <n.nijenhuis@action.nl>
2026-05-15 11:52:38 +0200

    fix warnings

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Custom404() {
   return (
     <div style={{ textAlign: 'center', padding: '50px 20px' }}>
       <h1>404 - Page not found</h1>
-      <p>The page you're looking for doesn't exist.</p>
-      <a href="/">Back to home</a>
+      <p>The page you&apos;re looking for doesn&apos;t exist.</p>
+      <Link href="/">Back to home</Link>
     </div>
   )
 }
